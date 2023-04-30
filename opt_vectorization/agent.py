1da379d5e505defe1342fb82d9023d3a904ab649
Chloe Zheng <c.zheng98@gmail.com>
2023-04-30 13:58:54 -0400

    add optimization methods for vectorization
    actual changes:
    - transition - infect(...)
    - basic_sim - main()
    - random_vaccine_sim - main()

---
+++ opt_vectorization/agent.py
@@ -0,0 +1,23 @@
+class Agent:
+    def __init__(self, status, location, days_with_status=0, vaccinated=False, vaccine_efficacy=0, days_since_vaccination=0):
+        self.status = status
+        self.location = location
+        self.days_with_status = days_with_status
+        self.vaccinated = vaccinated
+        self.vaccine_efficacy = vaccine_efficacy
+        self.days_since_vaccination = days_since_vaccination
+
+    def reset_days_with_status(self):
+        self.days_with_status = 0
+
+    def increase_days_with_status(self):
+        self.days_with_status += 1
+        if self.vaccinated:
+            self.days_since_vaccination += 1
+            self.update_vaccine_efficacy()
+
+    def update_vaccine_efficacy(self):
+        # Update vaccine efficacy based on time since vaccination
+        if self.days_since_vaccination > 180:
+            self.vaccine_efficacy *= 0.98 # after one year, the efficacy is 0.95 * 0.98**185 = 2.26%
+
--- optimization_time.py
+++ optimization_time.py
@@ -1,16 +1,49 @@
 import timeit
+import sys
 
-def time_opt_cython():
-    print("Timing Optimization Method: Cython")
-    python_command = "python basic_sim.py 100 1000 0.05 0.3 7 0.3"
-    cython_command = "python opt_cython/basic_sim.py 100 1000 0.05 0.3 7 0.3"
+def time_opt(command, number=1):
+    print("-"*50,"\nTiming Original Method")
+    python_command = "python " + command
+    python = timeit.timeit("os.system('{}')".format(python_command), setup='import os', number=number)
+    print("Python Time: {} sec".format(python))
 
-    python = timeit.timeit("os.system('{}')".format(python_command), setup='import os', number=5)
-    cython = timeit.timeit("os.system('{}')".format(cython_command), setup='import os',number=5)
-    print()
-    print("Python Time: ", python)
-    print("Cython Time: ", cython)
+    print("-"*50,"\nTiming Optimization Method: Cython")
+    cython_command = "python opt_cython/" + command
+    cython = timeit.timeit("os.system('{}')".format(cython_command), setup='import os',number=number)
+    print("Cython Time: {} sec".format(cython))
     print(f"{python/cython}x times faster")
 
+    print("-"*50,"\nTiming Optimization Method: Vectorization")
+    vectorization_command = "python opt_vectorization/" + command
+    vectorization = timeit.timeit("os.system('{}')".format(vectorization_command), setup='import os',number=number)
+    print("Vectorization Time: {} sec".format(vectorization))
+    print(f"{python/vectorization}x times faster")
+    
+
 if __name__ == "__main__":
-    time_opt_cython()
+    command = sys.argv[1] # basic_sim.py, random_vaccine_sim.py, etc.
+    duration = int(sys.argv[2])
+    num_agents = int(sys.argv[3])
+    infection_distance = float(sys.argv[4])
+    infection_probability = float(sys.argv[5])
+    minimum_infection_duration = int(sys.argv[6])
+    recovery_probability = float(sys.argv[7])
+    vaccine_availability_day = ""
+    daily_vaccine_distribution_count = ""
+    initial_vaccine_efficacy = ""
+    vaccinated_recovery_reduction = ""
+
+
+    if len(sys.argv) > 8:
+        vaccine_availability_day = int(sys.argv[8])
+        daily_vaccine_distribution_count = int(sys.argv[9])
+        initial_vaccine_efficacy = float(sys.argv[10])
+        vaccinated_recovery_reduction = int(sys.argv[11])
+    
+
+    command += " {} {} {} {} {} {} {} {} {} {}".format(duration, num_agents, infection_distance, infection_probability, 
+                                           minimum_infection_duration, recovery_probability, 
+                                           vaccine_availability_day, daily_vaccine_distribution_count, 
+                                           initial_vaccine_efficacy, vaccinated_recovery_reduction)
+
+    time_opt(command)
