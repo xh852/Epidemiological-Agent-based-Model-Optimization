c7ba2721f01fd35d7f9f9a66ca9c6490b7d2c5f3
Chloe Zheng <c.zheng98@gmail.com>
2023-05-01 14:25:13 -0400

    Combined cython and vectorization
    - new folder "opt_cython+vec" all changes in cython and vectorization combined
    - fixed some ctypes in opt_cython too
    - added to optimization py file

--- opt_cython+vect/random_vaccine_sim.py
+++ opt_cython+vect/random_vaccine_sim.py
@@ -2,14 +2,15 @@ import sys
 import matplotlib.pyplot as plt
 import random
 import time
+import numpy as np
 from agent import Agent
 from transition import infect, recover, distribute_random_vaccine
 from location import generate_random_location, snap_to_edge
 
 def main(duration, num_agents, infection_distance, infection_probability, minimum_infection_duration, recovery_probability, vaccine_availability_day, daily_vaccine_distribution_count, initial_vaccine_efficacy=0.95, vaccinated_recovery_reduction=2):
-    # Initialize random seed
+   # Initialize random seed
     random.seed(42)   
-    
+
     # Initialize the list of agents
     agents = [Agent("S", (random.random(), random.random())) for _ in range(num_agents)]
 
@@ -17,19 +18,19 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
     agents[0].status = "I"
 
     # Initialize status counts
-    status_counts = {"S": [], "I": [], "R": []}
+    status_counts = {"S": np.zeros(duration+1), "I": np.zeros(duration+1), "R": np.zeros(duration+1)}
 
     # Run simulation for given duration
     for day in range(duration):
-        # Update status counts for current day
-        for status in ["S", "I", "R"]:
-            count = sum(1 for agent in agents if agent.status == status)
-            status_counts[status].append(count)
+         # Update status counts for current day
+        status_counts["S"][day] = np.count_nonzero([agent.status == "S" for agent in agents])
+        status_counts["I"][day] = np.count_nonzero([agent.status == "I" for agent in agents])
+        status_counts["R"][day] = np.count_nonzero([agent.status == "R" for agent in agents])
 
         # Update agent days with status and locations
+        max_distance = 0.01
         for agent in agents:
             agent.increase_days_with_status()
-            max_distance = 0.01
             new_location = generate_random_location(agent.location, max_distance)
             new_location = snap_to_edge(new_location, 0, 0, 1, 1)
             agent.location = new_location
@@ -43,10 +44,10 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
         # Recover agents
         recover(agents, minimum_infection_duration, recovery_probability, vaccinated_recovery_reduction)
 
-    # Add final day status counts
-    for status in ["S", "I", "R"]:
-        count = sum(1 for agent in agents if agent.status == status)
-        status_counts[status].append(count)
+    # Update status counts for last day
+    status_counts["S"][duration] = np.count_nonzero([agent.status == "S" for agent in agents])
+    status_counts["I"][duration] = np.count_nonzero([agent.status == "I" for agent in agents])
+    status_counts["R"][duration] = np.count_nonzero([agent.status == "R" for agent in agents])
 
     # Plot status counts over time
     plt.plot(status_counts["S"], label="Susceptible")
@@ -56,8 +57,7 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
     plt.ylabel("Number of Agents")
     plt.title("Agent-based Simulation")
     plt.legend()
-    plt.savefig("plot_random_vaccine_sim_optcyt_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.png".format(duration, num_agents, infection_distance, infection_probability, minimum_infection_duration, recovery_probability, vaccine_availability_day, daily_vaccine_distribution_count, initial_vaccine_efficacy, vaccinated_recovery_reduction))
-
+    plt.savefig("plot_random_vaccine_sim_optcytvec_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.png".format(duration, num_agents, infection_distance, infection_probability, minimum_infection_duration, recovery_probability, vaccine_availability_day, daily_vaccine_distribution_count, initial_vaccine_efficacy, vaccinated_recovery_reduction))
 
 if __name__ == "__main__":
     duration = int(sys.argv[1])
--- opt_cython+vect/targeted_vaccine_sim.py
+++ opt_cython+vect/targeted_vaccine_sim.py
@@ -2,6 +2,7 @@ import sys
 import matplotlib.pyplot as plt
 import random
 import time
+import numpy as np
 from agent import Agent
 from transition import infect, recover, distribute_targeted_vaccine
 from location import generate_random_location, snap_to_edge
@@ -17,7 +18,7 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
     agents[0].status = "I"
 
     # Initialize status counts
-    status_counts = {"S": [], "I": [], "R": []}
+    status_counts = {"S": np.zeros(duration+1), "I": np.zeros(duration+1), "R": np.zeros(duration+1)}
 
     for agent in agents:
         if agent.immunodeficient == True:
@@ -26,14 +27,14 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
     # Run simulation for given duration
     for day in range(duration):
         # Update status counts for current day
-        for status in ["S", "I", "R"]:
-            count = sum(1 for agent in agents if agent.status == status)
-            status_counts[status].append(count)
+        status_counts["S"][day] = np.count_nonzero([agent.status == "S" for agent in agents])
+        status_counts["I"][day] = np.count_nonzero([agent.status == "I" for agent in agents])
+        status_counts["R"][day] = np.count_nonzero([agent.status == "R" for agent in agents])
 
         # Update agent days with status, locations, and targetability
+        max_distance = 0.01
         for agent in agents:
             agent.increase_days_with_status()
-            max_distance = 0.01
             new_location = generate_random_location(agent.location, max_distance)
             new_location = snap_to_edge(new_location, 0, 0, 1, 1)
             agent.location = new_location
@@ -49,10 +50,10 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
         # Recover agents
         recover(agents, minimum_infection_duration, recovery_probability, vaccinated_recovery_reduction)
 
-    # Add final day status counts
-    for status in ["S", "I", "R"]:
-        count = sum(1 for agent in agents if agent.status == status)
-        status_counts[status].append(count)
+    # Update status counts for last day
+    status_counts["S"][duration] = np.count_nonzero([agent.status == "S" for agent in agents])
+    status_counts["I"][duration] = np.count_nonzero([agent.status == "I" for agent in agents])
+    status_counts["R"][duration] = np.count_nonzero([agent.status == "R" for agent in agents])
 
     # Plot status counts over time
     plt.plot(status_counts["S"], label="Susceptible")
@@ -62,7 +63,7 @@ def main(duration, num_agents, infection_distance, infection_probability, minimu
     plt.ylabel("Number of Agents")
     plt.title("Agent-based Simulation")
     plt.legend()
-    plt.savefig("plot_targeted_vaccine_sim_optcyt_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.png".format(duration, num_agents, infection_distance, infection_probability, minimum_infection_duration, recovery_probability, vaccine_availability_day, daily_vaccine_distribution_count, initial_vaccine_efficacy, vaccinated_recovery_reduction, immunodeficient_proportion, infection_probability_increase, complete_rollout_day))
+    plt.savefig("plot_targeted_vaccine_sim_optcytvec_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}.png".format(duration, num_agents, infection_distance, infection_probability, minimum_infection_duration, recovery_probability, vaccine_availability_day, daily_vaccine_distribution_count, initial_vaccine_efficacy, vaccinated_recovery_reduction, immunodeficient_proportion, infection_probability_increase, complete_rollout_day))
 
 if __name__ == "__main__":
     duration = int(sys.argv[1])
